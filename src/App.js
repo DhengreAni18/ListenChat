f76370f87879908328deae3ef90bb2fa5e047dbd
DhengreAni18 <dhengreani18@gmail.com>
2020-04-29 22:19:50 +0530

    Added Input area inside Cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from './logo.svg';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <article className="card">
+      <input type="text" name="name" height="100"/>
+      <br></br>
+      <input type="text" name="name" />
+      <br></br>
+      </article>
+
+      
       </header>
     </div>
   );

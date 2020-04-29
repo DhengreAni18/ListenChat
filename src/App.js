04ec4b8a5df13bae99d9d789de574bfa4f8ce02c
DhengreAni18 <dhengreani18@gmail.com>
2020-04-29 22:34:55 +0530

    Added React Speech

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
 // import logo from './logo.svg';
 import "./App.css";
+import Speech from 'react-speech';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       <article className="card">
+      Input Box 1
       <input type="text" name="name" height="100"/>
       <br></br>
+      Input Box 2
       <input type="text" name="name" />
       <br></br>
+      <Speech text="Hello World" />
       </article>
 
       

25e14c0a0b91e9ab6278dfd1b817a68a7ad1222f
DhengreAni18 <dhengreani18@gmail.com>
2020-04-30 21:29:06 +0530

    Added dynamic speech based on input

--- src/App.js
+++ src/App.js
@@ -1,27 +1,79 @@
 import React from "react";
 // import logo from './logo.svg';
 import "./App.css";
-import Speech from 'react-speech';
+import Speech from "react-speech";
 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      topicBox: null,
+      payloadBox: null,
+    };
 
-function App() {
-  return (
-    <div className="App">
+    this.publish = this.publish.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({
+      [target.name]: target.value,
+    });
+  }
+
+  publish() {
+    console.log(this.state.topicBox, this.state.payloadBox);
+  }
+
+  render() {
+    return (
       <header className="App-header">
-      <article className="card">
-      Input Box 1
-      <input type="text" name="name" height="100"/>
-      <br></br>
-      Input Box 2
-      <input type="text" name="name" />
-      <br></br>
-      <Speech text="Hello World" />
-      </article>
+        <article className="card">
+          <div>
+            <div class="grid2x2">
+              <div class="box box1">
+                <div class="form__group field">
+                  <input
+                    type="input"
+                    class="form__field"
+                    placeholder="Name"
+                    name="topicBox"
+                    id="name"
+                    value={this.state.topicBox}
+                    onChange={this.handleChange}
+                  />
+                  <label for="name" class="form__label">
+                    BOT
+                  </label>
+                </div>
+              </div>
+              <div class="box box2">
+                <div class="form__group field">
+                  <input
+                    type="input"
+                    class="form__field"
+                    placeholder="Name"
+                    name="payloadBox"
+                    id="name"
+                    value={this.state.payloadBox}
+                    onChange={this.handleChange}
+                  />
+                  <label for="name" class="form__label">
+                    CUSTOMER
+                  </label>
+                </div>
+              </div>
+            </div>
 
-      
+            {/* <button value="Send" onClick={this.publish}>
+              Publish
+            </button>  */}
+            <Speech text={this.state.topicBox} textAsButton displayText="Play" />
+          </div>
+        </article>
       </header>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;

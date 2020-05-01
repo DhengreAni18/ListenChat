9ac150d8f0b848dfb7fed06084d836add4e7ed55
DhengreAni18 <dhengreani18@gmail.com>
2020-05-01 22:34:00 +0530

    Updates

---
+++ precache-manifest.f2d2ce4bb5512094addf30f11a4fd967.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03e13ab43cef8661d4436bfcd983d8f7",
+    "url": "/ReactJS-Interface/index.html"
+  },
+  {
+    "revision": "e5f5eadd47e6a6b14b31",
+    "url": "/ReactJS-Interface/static/css/main.c6625b3d.chunk.css"
+  },
+  {
+    "revision": "73cfa54f99c85b477633",
+    "url": "/ReactJS-Interface/static/js/2.4ac0dc1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJS-Interface/static/js/2.4ac0dc1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5f5eadd47e6a6b14b31",
+    "url": "/ReactJS-Interface/static/js/main.73f35515.chunk.js"
+  },
+  {
+    "revision": "a9ba790385c7714a415d",
+    "url": "/ReactJS-Interface/static/js/runtime-main.a38d4f48.js"
+  }
+]);
